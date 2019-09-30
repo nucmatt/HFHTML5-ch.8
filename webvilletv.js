5d37908907b67b438336c7759a21048d57de3419
Matthew Wessel <nucmatt@gmail.com>
2019-09-30 13:27:21 -0500

    initial commit

---
+++ webvilletv.js
@@ -0,0 +1,37 @@
+//Track which video is playing
+var position = 0;
+
+//playlist array
+var playlist;
+
+//reference to the video element
+var video;
+
+window.onload = function() {
+    playlist = ["video/preroll", "video/areyoupopular", "video/destinationearth"];
+    video = document.getElementById('video'),
+    video.addEventListener('ended', nextVideo, false)
+    video.src = playlist[position] + this.getFormatExtension();
+    video.load();
+    video.play();
+}
+
+function nextVideo() {
+    position++;
+    if (position >= playlist.length) {
+        position = 0;
+    }
+    video.src = playlist[position];
+    video.load();
+    video.play();
+}
+
+function getFormatExtension() {
+    if (video.canPlayType("video/mp4") != "") {
+        return ".mp4";
+    } else if (video.canPlayType("video/webm") != "") {
+        return ".webm";
+    } else if (video.canPlayType("video/ogg") != "") {
+        return ".ogv";
+    }
+}
